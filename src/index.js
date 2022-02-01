81c691e13b9c6b693d518f244e5a38809c9ae56c
Real Ivan Nosov <i-am-ivan-nosov@yandex.com>
2022-02-01 20:36:05 +0300

    base

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log('ff');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const TodoList = () => {
+  return (
+    <ul>
+      <li>Eat</li>
+      <li>Sleep</li>
+    </ul>
+  )
+
+}
+
+const AppHeader = () => {
+  return (
+    <h1>Todo List</h1>
+  )
+
+}
+
+const SearchPanel = () => {
+  return (
+    <input placeholder='search' />
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
